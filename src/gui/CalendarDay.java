d4b1902fed2eda10f22156e1fd87eb095264e477
annesofiemcnair <annesofiemcnairrahbek@gmail.com>
2014-11-27 09:01:42 +0100

    forsøg

--- src/gui/CalendarDay.java
+++ src/gui/CalendarDay.java
@@ -36,7 +36,7 @@ public class CalendarDay {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(150, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
