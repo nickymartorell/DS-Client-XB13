577031d150301641047d26b9cd272d0d611dad46
nickymartorell <nicky.martorell.privat@gmail.com>
2014-12-03 15:09:48 +0100

    Serverconnection

--- src/shared/ServerConnection.java
+++ src/shared/ServerConnection.java
@@ -1,13 +1,45 @@
 package shared;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class ServerConnection {
 
-	public Object getCalendarObject() {
-		
-		return null;
+	private String serverReply; 
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	private Socket clientSocket;
+
+	public void connect(){
+
+		try {
+			clientSocket = new Socket("localhost", 8888);
+
+			input = new ObjectInputStream(clientSocket.getInputStream());
+			output = new ObjectOutputStream(clientSocket.getOutputStream());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-	
-	
-	
+	public String getFromServer(String gsonString) throws UnknownHostException, IOException, ClassNotFoundException{
+
+		output.writeObject(gsonString);
+		output.flush();
+		serverReply = (String) input.readObject();
+		return serverReply; 
+
+	}
+	public void close() throws IOException{
+		clientSocket.close();
+	}
+
 }
