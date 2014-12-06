2977c60a6fa6fb5c85dbcedd72301e820b06bbbf
annesofiemcnair <annesofiemcnairrahbek@gmail.com>
2014-12-06 17:42:50 +0100

    nfjskn

--- src/gui/CalendarDay.java
+++ src/gui/CalendarDay.java
@@ -1,13 +1,10 @@
-import javax.swing.JPanel;
-
-import GUI.Screen;
-import Logic.ActionController;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
@@ -28,9 +25,6 @@ import java.awt.Color;
 
 public class CalendarDay extends JPanel{
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	public static final String WEEK = "week";
 	public static final String NOTE = "note";
@@ -40,207 +34,61 @@ public class CalendarDay extends JPanel{
 	public static final String DELNOTE = "delnote";
 	
 	private JLabel lblDayView;
-	private JPanel eventPanel;
-	private JPanel activePanel;
-	private JTable eventTable;
-	private JScrollPane eventScroll;
+//	private JScrollPane eventScroll;
 	private JButton btnCreate;
 	private JButton btnDelete;
 	private JButton btnBack;
 	private JButton btnNote;
-	private JButton btnSet;
+	private JButton btnnewNote;
 	private JButton btnDelNote;
-	private JTextArea forecastTxt;
-	private JTextArea noteTxt;
-	private JPanel bottomPanel;
-	private JPanel infoPanel;
-	private JPanel notePanel;
+	//private JTextArea forecastTxt;
+	//private JTextArea noteTxt;;
 	
-	public CalendarDay () {
-		setSize(Screen.WIDTH,Screen.HEIGHT);
-		setLayout(new BorderLayout());
+	public CalendarDay(){
+		
 		setBackground(new Color(199, 21, 133));
-	
+		setLayout(null);
 		
-		lblDayView = new JLabel("Day view");
-		lblDayView.setHorizontalAlignment(SwingConstants.CENTER);
-		add(lblDayView, BorderLayout.NORTH);
+		lblDayView = new JLabel("Hello, here's your calendar!");
+		lblDayView.setBounds(48, 23, 354, 25);
+		lblDayView.setForeground(new Color(255, 192, 203));
+		lblDayView.setFont(new Font("Tahoma", Font.BOLD, 25));
+		add(lblDayView);
 		
+		btnBack = new JButton ("Back to menu");
+		btnBack.setBounds(149, 229, 152, 29);
+		btnBack.setForeground(new Color(255, 192, 203));
+		btnBack.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(btnBack);
 		
-		eventPanel = new JPanel();
-		add(eventPanel, BorderLayout.CENTER);
-		eventPanel.setLayout(new BorderLayout(0, 0));
-		setBackground(new Color(199, 21, 133));
-	
-		
-		notePanel = new JPanel();
-		notePanel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(199, 21, 133), SystemColor.textHighlight, SystemColor.textHighlight, SystemColor.textHighlight));
-//		panel.setBorder(new EmptyBorder(30, 30, 30, 30));
-		notePanel.setMaximumSize(new Dimension(1000, 100));
-		notePanel.setPreferredSize(new Dimension(300, 100));
-		notePanel.setMinimumSize(new Dimension(1, 100));
-		notePanel.setVisible(false);
-		eventPanel.add(notePanel, BorderLayout.EAST);
-		setBackground(new Color(199, 21, 133));
-		
-		
-		noteTxt = new JTextArea("");
-		noteTxt.setBackground(SystemColor.control);
-		noteTxt.setEditable(false);
-		noteTxt.setWrapStyleWord(true);
-	    noteTxt.setLineWrap(true);
-	    noteTxt.setVisible(false);
-		notePanel.add(noteTxt);
-		
-		
-		bottomPanel = new JPanel();
-		add(bottomPanel, BorderLayout.SOUTH);
-		bottomPanel.setLayout(new BorderLayout(0, 0));
-		setBackground(new Color(199, 21, 133));
-		
-		
-		activePanel = new JPanel();
-		bottomPanel.add(activePanel, BorderLayout.SOUTH);
-		setBackground(new Color(199, 21, 133));
-		
-		
-		btnCreate = new JButton("Add event");
-		btnCreate.setActionCommand(CREATEEVE);
-		activePanel.add(btnCreate);
+		btnCreate = new JButton("Create new event");
+		btnCreate.setForeground(new Color(255, 192, 203));
+		btnCreate.setBounds(149, 206, 152, 29);
+		btnCreate.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(btnCreate);
 		
 		btnDelete = new JButton("Delete event");
-		btnDelete.setActionCommand(DELETEEVE);
-		activePanel.add(btnDelete);
-		
-		btnBack = new JButton("Week View");
-		btnBack.setActionCommand(WEEK);
-		activePanel.add(btnBack);
+		btnDelete.setForeground(new Color(255, 192, 203));
+		btnDelete.setBounds(252, 176, 106, 29);
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(btnDelete);
 		
 		btnNote = new JButton("View note");
-		btnNote.setActionCommand(NOTE);
-		activePanel.add(btnNote);
+		btnNote.setForeground(new Color(255, 192, 203));
+		btnNote.setBounds(252, 146, 106, 29);
+		btnNote.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(btnNote);
 		
-		infoPanel = new JPanel();
-		bottomPanel.add(infoPanel, BorderLayout.NORTH);
-		infoPanel.setLayout(new GridLayout(0, 4, 0, 0));
-		setBackground(new Color(199, 21, 133));
+		btnnewNote = new JButton("Create new note");
+		btnnewNote.setForeground(new Color(255, 192, 203));
+		btnnewNote.setBounds(252, 124, 106, 29);
+		btnnewNote.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(btnnewNote);
 		
-		
-		forecastTxt = new JTextArea("");
-		forecastTxt.setEditable(false);
-		forecastTxt.setWrapStyleWord(true);
-		forecastTxt.setLineWrap(true);
-		forecastTxt.setBackground(SystemColor.control);
-		infoPanel.add(forecastTxt);
-		
-		btnSet = new JButton("Add note");
-		infoPanel.add(btnSet);
-		btnSet.setVisible(false);
-		btnSet.setActionCommand(SET);
-		
-		btnDelNote = new JButton("Delete Note");
-		btnDelNote.setVisible(false);
-		btnDelNote.setActionCommand(DELNOTE);
-		infoPanel.add(btnDelNote);
-		
-	
-		
+		btnDelNote = new JButton("Delete note");
+		btnDelNote.setForeground(new Color(255, 192, 203));
+		btnDelNote.setBounds(222, 72, 136, 29);
+		btnDelNote.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		add(btnDelNote);
 	}
-
-	
-
-	public void updateEvents(Object[][] data,String[] header) {
-		
-		if(eventTable != null){
-			eventPanel.remove(eventTable);
-			eventPanel.remove(eventScroll);
-		}
-		eventTable = new JTable(data,header);
-		eventTable.setPreferredScrollableViewportSize(new Dimension(800,70));
-		eventTable.setFillsViewportHeight(true);
-		
-		eventScroll = new JScrollPane(eventTable);
-		eventScroll.setBounds(26,30,930,280);
-		eventPanel.add(eventScroll);
-		
-	}
-	
-	public void removeTable() {
-		eventPanel.remove(eventTable);
-		eventPanel.remove(eventScroll);
-	}
-	
-	public JLabel getLblDayView() {
-		return lblDayView;
-	}
-
-	public void setLblDayView(JLabel lblDayView) {
-		this.lblDayView = lblDayView;
-	}
-
-	public JPanel getEventPanel() {
-		return eventPanel;
-	}
-
-	public void setEventPanel(JPanel eventPanel) {
-		this.eventPanel = eventPanel;
-	}
-
-	public JPanel getActivePanel() {
-		return activePanel;
-	}
-	public JTextArea getForecastTxt() {
-		return forecastTxt;
-	}
-
-	public void setForecastTxt(JTextArea forecastTxt) {
-		this.forecastTxt = forecastTxt;
-	}
-
-	public void setActivePanel(JPanel activePanel) {
-		this.activePanel = activePanel;
-	}
-
-	public JTextArea getNoteLbl() {
-		return noteTxt;
-	}
-
-	public void setNoteTxt(JTextArea noteTxt) {
-		this.noteTxt = noteTxt;
-	}
-
-	public JButton getBtnSet() {
-		return btnSet;
-	}
-
-	public void setBtnSet(JButton btnSet) {
-		this.btnSet = btnSet;
-	}
-
-	
-
-
-
-	public JPanel getNotePanel() {
-		return notePanel;
-	}
-
-
-
-	public void setNotePanel(JPanel notePanel) {
-		this.notePanel = notePanel;
-	}
-
-
-
-	public JButton getBtnDelNote() {
-		return btnDelNote;
-	}
-
-
-
-	public void setBtnDelNote(JButton btnDelNote) {
-		this.btnDelNote = btnDelNote;
-	}
-
 }
