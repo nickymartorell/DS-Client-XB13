f8ad258a826e2437b13aaf2278e0ebf429d5cdf4
nickymartorell <nicky.martorell.privat@gmail.com>
2014-12-07 12:25:43 +0100

    Commit

--- src/shared/ServerConnection.java
+++ src/shared/ServerConnection.java
@@ -1,54 +1,45 @@
 package shared;
- 
-import gui.Screen;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.net.UnknownHostException;
 
-
- 
-
-
-
-
-import logic.ActionController;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
- 
-
- 
-@SuppressWarnings("unused")
 public class ServerConnection {
-	
-       
-        public String sendMessage(String gsonString) throws Exception {
-                Encryption cryp = new Encryption();
-                String stringToBeReturned = null;
-                String modifiedSentence;
-                Socket clientSocket = new Socket("localhost", 8888);
-                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-               
-                byte[] encrypted = cryp.decrypt(gsonString.getBytes());
-                outToServer.write(encrypted);
-                System.out.println("FROM CLIENT:"+encrypted);
-                outToServer.flush();
-                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                modifiedSentence = inFromServer.readLine();
-                System.out.println("SE HER FRA SERVER: "+modifiedSentence);
-                String TestingString = new String(modifiedSentence).trim();
-               
-                byte[] decrypted = cryp.decrypt(TestingString.getBytes());
-               
-                String decrypted2 = new String(decrypted).trim();
-                System.out.println("FROM SERVER: " + decrypted2);
-                clientSocket.close();
-                stringToBeReturned = decrypted2;
-               
-                return stringToBeReturned;
-        }
+
+	private String serverReply; 
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	private Socket clientSocket;
+
+	public void connect(){
+
+		try {
+			clientSocket = new Socket("localhost", 8888);
+
+			input = new ObjectInputStream(clientSocket.getInputStream());
+			output = new ObjectOutputStream(clientSocket.getOutputStream());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getFromServer(String gsonString) throws UnknownHostException, IOException, ClassNotFoundException{
+
+		output.writeObject(gsonString);
+		output.flush();
+		serverReply = (String) input.readObject();
+		return serverReply; 
+
+	}
+	public void close() throws IOException{
+		clientSocket.close();
+	}
+
 }
