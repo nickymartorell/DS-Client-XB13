b8bfe5ecd2c95257548a2145be0d15c284ba9a4e
nickymartorell <nicky.martorell.privat@gmail.com>
2014-12-07 13:08:33 +0100

    ..

--- src/shared/ServerConnection.java
+++ src/shared/ServerConnection.java
@@ -5,8 +5,11 @@ import gui.Screen;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.ArrayList;
+
 import logic.ActionController;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
